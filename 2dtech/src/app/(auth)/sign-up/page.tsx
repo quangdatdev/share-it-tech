0e2b8ea96270dcec8f3d9299a611f53862191306
Pham Dat <phamdat02012001@gmail.com>
2023-10-12 18:48:51 +0700

    finished authen, create community, building user's profile menu

--- 2dtech/src/app/(auth)/sign-up/page.tsx
+++ 2dtech/src/app/(auth)/sign-up/page.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/Button";
 import SignIn from "@/components/SignIn";
 import { ChevronLeft } from "lucide-react";
+import SignUp from "@/components/SignUp";
 
 const page: FC = ({}) => {
   return (
@@ -20,7 +21,7 @@ const page: FC = ({}) => {
           Home
         </Link>
 
-        <SignIn />
+        <SignUp />
       </div>
     </div>
   );
--- 2dtech/src/app/layout.tsx
+++ 2dtech/src/app/layout.tsx
@@ -1,4 +1,6 @@
 import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
+import { Toaster } from "@/components/ui/Toaster";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
@@ -12,8 +14,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
+  authModal,
 }: {
   children: React.ReactNode;
+  authModal: React.ReactNode;
 }) {
   return (
     <html
@@ -24,10 +28,17 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-        <Navbar />
+        <Providers>
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
 
-        <div className="container max-w-7x1 mx-auto h-full pt-12"></div>
-        {children}
+          {authModal}
+
+          <div className="container max-w-7x1 mx-auto h-full pt-12">
+            {children}
+          </div>
+        </Providers>
+        <Toaster />
       </body>
     </html>
   );
