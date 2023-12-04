c90abb278fd8432c194f658dd3cee32f6679bec6
Pham Dat <phamdat02012001@gmail.com>
2023-12-04 18:45:50 +0700

    fix loi hien thi feed

--- 2dtech/src/components/CustomFeed.tsx
+++ 2dtech/src/components/CustomFeed.tsx
@@ -1,14 +1,19 @@
 import { INFINITE_SCROLL_PAGINATION_RESULTS } from "@/config";
-import { db } from "@/lib/db";
-import PostFeed from "./PostFeed";
 import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+
+import { notFound } from "next/navigation";
+import PostFeed from "./PostFeed";
 
 const CustomFeed = async () => {
   const session = await getAuthSession();
 
+  // only rendered if session exists, so this will not happen
+  if (!session) return notFound();
+
   const followedCommunities = await db.subscription.findMany({
     where: {
-      userId: session?.user.id,
+      userId: session.user.id,
     },
     include: {
       subreddit: true,
@@ -19,7 +24,7 @@ const CustomFeed = async () => {
     where: {
       subreddit: {
         name: {
-          in: followedCommunities.map(({ subreddit }) => subreddit.id),
+          in: followedCommunities.map((sub) => sub.subreddit.name),
         },
       },
     },
@@ -34,6 +39,7 @@ const CustomFeed = async () => {
     },
     take: INFINITE_SCROLL_PAGINATION_RESULTS,
   });
+
   return <PostFeed initialPosts={posts} />;
 };
 
